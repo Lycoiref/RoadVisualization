b7a59375bea4bec6e2a45875c7aaf9204ecf2c04
Lycoiref <1808347356@qq.com>
2023-04-20 20:03:12 +0800

    添加echarts

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import 'dotenv'
-import App from './App.vue'
 // 引入vue-amap
-import VueAMap, { initAMapApiLoader } from '@vuemap/vue-amap'
 import '@vuemap/vue-amap/dist/style.css'
+import './style.css'
+import App from './App.vue'
+import VueAMap, { initAMapApiLoader } from '@vuemap/vue-amap'
+import { createApp } from 'vue'
 
-console.log('111')
 // 初始化vue-amap
 initAMapApiLoader({
     // 高德的key
-    key: process.env.AMAP_API_KEY,
-    securityJsCode: 'securityJsCode', // 新版key需要配合安全密钥使用
+    key: import.meta.env.VITE_AMAP_API_KEY,
+    securityJsCode: import.meta.env.VITE_SECURITY_CODE, // 新版key需要配合安全密钥使用
     //Loca:{
     //  version: '2.0.0'
     //} // 如果需要使用loca组件库，需要加载Loca
